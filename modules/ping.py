25651ac9efde78ce69e3556d706c6b371a1e45c5
Matthew Ramina <mattramina@gmail.com>
2014-12-02 19:33:17 -0500

    Fixed .queue display by showing a list of queues when none is specified
    Conflicts:
    	modules/ping.py
    	modules/queue.py

--- modules/ping.py
+++ modules/ping.py
@@ -8,10 +8,34 @@ About: http://inamidst.com/phenny/
 import random
 
 def hello(phenny, input): 
+<<<<<<< HEAD
     greeting = random.choice(('Hi', 'Hey', 'Hello'))
     punctuation = random.choice(('', '!'))
     phenny.say(greeting + ' ' + input.nick + punctuation)
 hello.rule = r'(?i)(hi|hello|hey) $nickname[ \t]*$'
+=======
+    global temporaryMessage
+    global emptyHellos
+    global greetings
+
+    nickname = phenny.nick
+    greeting = random.choice(greetings)
+    if not re.search(nickname, input) and temporaryMessage is not None:
+        punctuation = random.choice(('.', '!', ';'))
+        phenny.say(greeting + ' ' + input.nick + punctuation +temporaryMessage)
+        countGuest(phenny, input.nick)
+    elif emptyHellos:
+        punctuation = random.choice(('', '!'))
+        phenny.say(greeting + ' ' + input.nick + punctuation)
+    elif re.search(nickname, input):
+        punctuation = random.choice(('', '!'))
+        phenny.say(greeting + ' ' + input.nick + punctuation)
+
+
+#hello.rule = r'(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|top o[\'f]* the mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|wb) $nickname[ \t]*$'
+#strings = "(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|top o[\'f]* the mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|s[ae]l[ao]m|wb)"
+hello.rule = r'(?:$nickname[,:]* )?(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|what\'s up|sup|(?:top o[\'f]* the )?mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|s[ae]l[ao]m|wb|mitä kuuluu|როგორა ხარ|ինչպե՞ս ես|сайн байна уу|कैसे हो|como vai|nasılsın(?:ız)?)(?:[,]* $nickname)?[ \t!\.\?]*$'
+>>>>>>> 508b470... Fixed .queue display by showing a list of queues when none is specified
 
 def interjection(phenny, input): 
     phenny.say(input.nick + '!')
